3482fe13c656170ae41b242ebe30ca337d83e265
esteve11 <rogeresteve30@gmail.com>
2020-09-21 19:32:05 +0200

    Basic gameplay

--- Entity.js
+++ Entity.js
@@ -1,28 +1,47 @@
 class Entity {
-    constructor(pos, id) {
+    constructor(id) {
         this.w = 50;
         this.pos = new Vector2(0, 0);
         this.pos.x = Math.floor(Math.random()*(width-this.w));
         this.pos.y = Math.floor(Math.random()*(height-this.w));
-        this.vel = new Vector2(Math.floor(Math.random()*2) == 0 ? -2 : 2, Math.floor(Math.random()*2) == 0 ? -2 : 2);
-    
-        if(!id) {
-            id = 0;
-        }
+        this.vel = this.randVel();
+        this.id = id;
+
+        this.maxvel = 1.5;
+
+        this.moveTime = 0;
+        this.maxMoveTime = 100;
     }
 
     update() {
+        if(this.moveTime > this.maxMoveTime) {
+            this.vel = this.randVel();
+            this.moveTime = 0;
+        }
+        this.moveTime++;
+
         this.pos.x += this.vel.x;
         this.pos.y += this.vel.y;
 
-        if(this.pos.x < 0) this.vel.x = 2;
-        if(this.pos.x > width-this.w) this.vel.x = -2;
-        if(this.pos.y < 0) this.vel.y = 2;
-        if(this.pos.y > height-this.w) this.vel.y = -2;
+        if(this.pos.x < 0) this.vel.x = this.maxvel;
+        if(this.pos.x > width-this.w) this.vel.x = -this.maxvel;
+        if(this.pos.y < 0) this.vel.y = this.maxvel;
+        if(this.pos.y > height-this.w) this.vel.y = -this.maxvel;
     }
 
     render() {
         ctx.fillStyle = color(255, 255, 255);
-        ctx.drawImage(images[0], this.pos.x, this.pos.y, this.w, this.w);
+        ctx.drawImage(images[this.id], this.pos.x, this.pos.y, this.w, this.w);
+    }
+
+    randVel() {
+        return new Vector2(Math.floor(Math.random()*2) == 0 ? -1.5 : 1.5, Math.floor(Math.random()*2) == 0 ? -1.5 : 1.5);
+    }
+
+    checkInside(x, y) {
+        if(x > this.pos.x && x < this.pos.x + this.w && y > this.pos.y && y < this.pos.y + this.w) {
+            return true;
+        }
+        return false;
     }
 }
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let
 canvas, ctx, 
-width = 800, height = 800,
+cw = 800, ch = 600;
+width = 600, height = 600,
 
-entities = [],
+entities = [], target_id = null,
+round = 0, n = 50,
 
-time_steps = 1
+time_steps = 1,
 
 images = [
     document.getElementById("esteve"),
@@ -17,18 +19,37 @@ images = [
 function start() {
     init();
 
-    for(let i = 0; i < 10; i++)
-        entities.push(new Entity(0, new Vector2(10, 10)));
+    startRound();
 
     window.requestAnimationFrame(loop);
 }
 
 function init() {
     canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = cw;
+    canvas.height = ch;
     ctx = canvas.getContext("2d");
     document.body.appendChild(canvas);
+
+    canvas.addEventListener("click", onclick);
+
+}
+
+function startRound() {
+    entities = [];
+    target_id = Math.floor(Math.random()*images.length);
+    console.log("Target ID: " + target_id);
+
+    for(let i = 0; i < n; i++) {
+        let id = getRandomID();
+        while(id === target_id) {
+            id = getRandomID();
+        }
+        
+        entities.push(new Entity(id));
+    }
+
+    entities.splice(Math.floor(Math.random()*n), 0, new Entity(target_id));
 }
 
 function loop() {
@@ -44,12 +65,37 @@ function update() {
 }
 
 function render() {
+    //Background
     ctx.fillStyle = color(255, 255, 255);
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(0, 0, cw, ch);
+
+    //Right border
+    ctx.fillStyle = color(0, 0, 0);
+    ctx.fillRect(width, 0, 5, height);
+
     for(i = 0; i < entities.length; i++) {
         entities[i].render();
     }
 
+    //Target
+    ctx.drawImage(images[target_id], 615, 10, 200, 200);
+}
+
+function onclick(e) {
+    let mx = e.offsetX, my = e.offsetY;
+
+    for(let i = 0; i < entities.length; i++) {
+        if(entities[i].checkInside(mx, my)) {
+            if(entities[i].id == target_id) {
+                round++;
+                startRound();
+            }
+        }
+    }
+}
+
+function getRandomID() {
+    return Math.floor(Math.random()*images.length);
 }
 
 function color(r, g, b) {
